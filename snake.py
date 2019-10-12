4e1e12b946e230adee4e36555e1ec56d4c1d4ec7
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2019-10-12 17:32:49 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 # SNAKES GAME
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+fifiglet Jopraveen | lolcat
 
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
